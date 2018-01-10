6b6d25f823d1a117e75be3c3356bbc3b6c5800f1
Cory <cory@cht.com.tw>
2018-01-10 00:49:44 +0000

    :memo: ssl note

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-        this.http.get('/blog/1.md', { responseType: 'text' })
+        this.http.get('/blog/2018_01_09_ssl_note.md', { responseType: 'text' })
             .subscribe(data => {
                 this.title = data;
             });
